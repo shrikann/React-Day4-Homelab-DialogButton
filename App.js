87b0fe90cdb47c437e0609a1082e511dfe77c388
SHRINES K <128037088+shrikann@users.noreply.github.com>
2023-03-29 12:13:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './App.css';
+import DialogButton from './DialogButton';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <DialogButton/>
+        </center>
+      </div>
+    )
+  }
+}
+export default App
